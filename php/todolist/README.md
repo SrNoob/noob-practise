d4afda4cdacc27a0464e08c7d6c4f0eb1303adce
herry <herry_tan@api-hk.com>
2018-01-24 19:07:30 +0800

    symfony - todolist

---
+++ php/todolist/README.md
@@ -0,0 +1,4 @@
+todolist
+========
+
+A Symfony project created on January 24, 2018, 9:14 am.
---
+++ php/todolist/database.sql
@@ -0,0 +1,5 @@
+INSERT INTO `todo` 
+(`id`, `name`, `category`, `description`, `priority`, `due_date`, `create_date`) 
+VALUES 
+(NULL, 'Pickup kids', 'Family', 'Pick up the kids from school', 'High', '2018-01-25 00:00:00', '2018-01-24 00:00:00'), 
+(NULL, 'Meeting with Team', 'Work', 'Meet with the team', 'Normal', '2018-01-26 00:00:00', '2018-01-24 00:00:00');
